13425d27c3a6c07785c7e7a7d04c191185e003cc
Jeeva <jeeva.n.dev01@gmail.com>
2026-03-01 11:21:40 +0530

    secondcommit

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,11 @@
 import React from "react";
 
 const Home = () => {
-  return
-    <div>Home
-  <p>Hello</p>
-  </div>;
+  return (
+    <div>
+      <p>Hello</p>
+    </div>
+  );
 };
 
 export default Home;
-
